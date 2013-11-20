a3e2dc3f27f83509a3b0f7954abbea540cb869cb
Stanley Zhang <stanley.zhang@ityin.net>
2013-11-20 17:11:32 +1300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flog::Application.config.secret_key_base = '9fe96f813a6c7da41c95ac2aa53e2c3a9ba5deed60d3987575d986744511a92210b379385b7ac697f2ddd37d3a1392f0b1bca853ca14f05dd703212f7723322a'
